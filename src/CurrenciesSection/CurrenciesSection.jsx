e6c4f142727eb810bd0ac683702571e551c44230
Dmitry <dimajanuary@gmail.com>
2025-06-23 02:22:17 +0300

    asd

--- src/CurrenciesSection/CurrenciesSection.jsx
+++ src/CurrenciesSection/CurrenciesSection.jsx
@@ -1,51 +1,67 @@
 import CurrenciesTable from "../AvailableCurrenciesTable/CurrenciesTable";
-import AddCurrencyForm from '../AddCurrencyForm/AddCurrencyForm';
-import { useState } from "react";
+import AddCurrencyForm from "../AddCurrencyForm/AddCurrencyForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import qs from "qs";
+
+const API = `http://localhost:8080/CurrencyExchanger-1.0/currencies`;
 
 export default function CurrenciesSection() {
-  var [currencies, setCurrencies] = useState([
-    {
-      id: 1,
-      name: "Australian dollar",
-      code: "AUD",
-      sign: "A$",
-    },
-    {
-      id: 2,
-      name: "Afgani",
-      code: "AFN",
-      sign: "Af",
-    },
-    {
-      id: 3,
-      name: "United States dollar",
-      code: "USD",
-      sign: "$",
-    },
-    {
-      id: 4,
-      name: "Euro",
-      code: "EUR",
-      sign: "€",
-    },
-  ]);
+  var [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    const fetchCurrencies = () => {
+      axios
+        .get(API)
+        .then((result) => {
+          setCurrencies(result.data);
+        })
+        .catch((error) => {
+          console.log("fetch error", error);
+        });
+    };
+
+    fetchCurrencies();
+  }, []);
 
   function addCurrency(name, code, sign) {
-    console.log("adding");
-    setCurrencies([
-      ...currencies,
-      {
-        name: name,
-        code: code,
-        sign: sign,
-      },
-    ]);
+    const postCurrency = (requestData) => {
+      axios
+        .post(API, requestData, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        })
+        .then((result) => {
+          console.log("adding responce", result.data);
+          setCurrencies([
+            ...currencies,
+            {
+              name: result.data.name,
+              code: result.data.code,
+              sign: result.data.sign,
+            },
+          ]);
+        })
+        .catch((error) => {
+          console.log("add error", error);
+        });
+    };
+
+    var requestData = qs.stringify({
+      name,
+      code,
+      sign,
+    });
+
+    postCurrency(requestData);
   }
+
   return (
     <section className="mt-xxl-5">
       <div className="row">
         <CurrenciesTable currencies={currencies} />
-        <AddCurrencyForm addCurrencyClick={addCurrency}/>
+        <AddCurrencyForm addCurrencyClick={addCurrency} />
       </div>
     </section>
   );
